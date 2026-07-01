a9725e5659b92a82d28d6f95f9a56e302da2ef79
viiveksahhu123 <viveksahuu900@gmail.com>
2026-07-01 11:09:19 +0530

    first set up commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPEN_E_DOT ='-e .'
+
+def get_requirments(file_path:str)->List[str]:
+    requirments=[]
+
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        requirments=[req.replace('\n',"")for req in requirments]
+        if HYPEN_E_DOT in requirments:
+          requirments.remove(HYPEN_E_DOT)  
+    return requirments
+
+
+setup( name='mlproject',
+      author='viivek',
+      version='0.0.1',
+    packages=find_packages(),
+    install_requires=get_requirments('requirment.txt')
+)
