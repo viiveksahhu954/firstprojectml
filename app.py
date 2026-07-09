80438c6c0b7c7eafac3d5e2f679931daa75a5c0a
viiveksahhu123 <viveksahuu900@gmail.com>
2026-07-09 12:05:54 +0530

    model tranier

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from src.mlproject.components.data_ignition import DataIgnition
 from src.mlproject.components.data_ignition import DataIgnitionConfi
 from src.mlproject.components.data_transformation import DataTransformationConfig
 from src.mlproject.components.data_transformation import DataTransformation
-
+from src.mlproject.components.model_tranier import ModelTrainerConfig
+from src.mlproject.components.model_tranier import ModelTrainer
 
 
 if __name__=="__main__":
@@ -17,7 +18,12 @@ if __name__=="__main__":
         train_data_path,test_data_path = data_ignition.initiate_data_ignition()
         ###data_transformation_confi=DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
 
     except Exception as e:
         logging.info('custom exception')
